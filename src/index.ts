234b91fd3128a64f0c755242614df7ec2def4fa2
Tymoteusz Strozniak <tymoteusz.strozniak@gmail.com>
2025-07-06 15:16:23 +0200

    feat: add CORS support and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { connectDB } from './config/db';
 import { authRouter } from './routes/auth.routes';
+import cors from 'cors';
+
 if (process.env.NODE_ENV !== 'production') {
   console.log('process.env.NODE_ENV', process.env.NODE_ENV);
   import('dotenv/config');
 }
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/api', authRouter);
 
 console.log('ENV-DEBUG  ➜  MONGODB_URI =', process.env.MONGODB_URI ?? '<undefined>');
