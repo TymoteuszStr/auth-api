dbb936648cdeb15a787ec0fe41298b952b19707b
Tymoteusz Strozniak <tymoteusz.strozniak@gmail.com>
2025-07-06 14:38:53 +0200

    tets - logs

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use('/api', authRouter);
 
+console.log('ENV-DEBUG  ➜  MONGODB_URI =', process.env.MONGODB_URI ?? '<undefined>');
+console.log('ENV-DEBUG  ➜  wszystkie klucze:', Object.keys(process.env));
+
 connectDB().then(() => {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`🚀 Server on http://localhost:${PORT}`));
